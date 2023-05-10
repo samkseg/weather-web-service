c255bf4b59bec8ad53ba898cbc7b1b2cd5f54827
Ken <ken.segawa@iths.se>
2023-05-10 09:51:01 +0200

    Added idea project

---
+++ src/main/java/se/iths/weatherwebservice/WeatherWebServiceApplication.java
@@ -0,0 +1,13 @@
+package se.iths.weatherwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/weatherwebservice/WeatherWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.weatherwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
